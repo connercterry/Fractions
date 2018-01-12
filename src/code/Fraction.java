3839c634f2e9a800be38b50b1f1b1f35a9abf1bc
Conner <connercterry22@yahoo.com>
2018-01-12 09:18:09 -0600

    added a simplify method a gcf method and changed the constructor to disallow a denom of 0.

--- src/code/Fraction.java
+++ src/code/Fraction.java
@@ -1,37 +1,67 @@
 package code;
 
-public class Fraction
-{
+public class Fraction {
     private int num, denom;
 
     public Fraction(int num, int denom)
     {
+        if(denom ==0)
+        {
+            throw new IllegalArgumentException("No you dummy. Fractions cannot have a zero in the denominator.");
+        }
+        else
+        {
         this.num = num;
         this.denom = denom;
+        simplify();
+        }
     }
-    public Fraction(int num)
+    public static int gcf(int a, int b)
     {
-        this.num=num;
+        int min = Math.min(a,b);
+        for(int x = min; x> 0; x--)
+        {
+            if(a%x ==0 && b%x==0) return x;
+
+        }
+        return 1;
+    }
+
+    public Fraction(int num) {
+        this.num = num;
         denom = 1;
     }
-    public Fraction(Fraction other)
-    {
+
+    public Fraction(Fraction other) {
         num = other.num;
         denom = other.denom;
     }
 
-    public String toString()
-    {
+    public String toString() {
         return num + "/" + denom;
     }
-    public Fraction inverse()
-    {
-       if(num == 0) return null;
-       return new Fraction(denom,num);
+
+    public Fraction inverse() {
+        if (num == 0) return null;
+        return new Fraction(denom, num);
     }
-    public Fraction multiply(Fraction stephen)
-    {
-        return new Fraction(num* stephen.num, denom*stephen.denom);
+
+    public Fraction multiply(Fraction stephen) {
+        return new Fraction(num * stephen.num, denom * stephen.denom);
+
     }
+
+    public Fraction divide(Fraction f)
+    {
+        return new Fraction(num * f.denom, denom * f.num);
+    }
+    private void simplify()
+    {
+        int factor = gcf(num,denom);
+        num/= factor;
+        denom/= factor;
+
+    }
+
 
 }
--- src/code/Runner.java
+++ src/code/Runner.java
@@ -4,18 +4,29 @@ public class Runner
 {
     public static void main(String[] args)
     {
-      Fraction myFraction = new Fraction(2, 3);
-      Fraction otherFraction = new Fraction(5);
-      Fraction thatFraction = new Fraction(myFraction);
-      Fraction stephens = myFraction.multiply(otherFraction);
-      Fraction goebel = new Fraction(0,21);
-      System.out.print(goebel.inverse());
-      goebel.multiply(goebel.inverse());
+      //Fraction myFraction = new Fraction(2, 0);
+      //Fraction otherFraction = new Fraction(5);
+     // Fraction thatFraction = new Fraction(myFraction);
+     // Fraction stephens = myFraction.multiply(otherFraction);
+     // Fraction goebel = new Fraction(0,21);
+      //System.out.print(goebel.inverse());
+      //goebel.multiply(goebel.inverse());
       //System.out.println(stephens);
       // System.out.println(myFraction);
      // System.out.println(otherFraction);
      // System.out.println(thatFraction);
-
+       /* Fraction f1=null;
+        try
+        {
+            f1 = new Fraction(3, 0);
+        }
+        catch(Exception e)
+        {
+            System.out.println("NOOOOOOOOOOO! Non-zero denominator needed big son.");
+        }
+       */ //System.out.println(f1);
+        Fraction myFraction = new Fraction(12, 4);
+        System.out.println(myFraction);
     }
 
 
